21f926c20d7ccd0abf53bf0da24308918e624086
Joseph EFTEKHARI <jeftekha@e1z2r3p42.42.us.org>
2017-04-16 01:37:13 -0700

    getnextline

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jeftekha <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/03/02 09:37:33 by jeftekha          #+#    #+#             */
+/*   Updated: 2017/03/20 15:43:51 by jeftekha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include "libft/libft.h"
+
+# define BUFF_SIZE 16
+# define MAX_FD 1025
+
+typedef struct		s_gnl
+{
+	struct s_gnl	*next;
+	struct s_gnl	*prev;
+	char			*c;
+	int				fd;
+}					t_gnl;
+
+int					get_next_line(const int fd, char **line);
+
+#endif
